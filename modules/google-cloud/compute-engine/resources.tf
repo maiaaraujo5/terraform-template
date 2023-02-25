76e1eaeee75b852ba8e0353f3f4d27a6f928d427
Lucas <maia.araujo5@gmail.com>
2023-02-25 16:33:10 -0300

    feat:add compute-engine

---
+++ modules/google-cloud/compute-engine/resources.tf
@@ -0,0 +1,28 @@
+resource "google_compute_instance" "instance" {
+  name         = var.name
+  machine_type = var.machine_type
+  allow_stopping_for_update = true
+  tags         = var.tags
+  zone         = var.region
+  boot_disk {
+    source = google_compute_disk.disk.name
+  }
+  network_interface {
+    network = "default"
+    access_config {
+      nat_ip = google_compute_address.static-ip-address.address
+    }
+  }
+}
+
+resource "google_compute_disk" "disk" {
+  name  = format("%s-%s", var.name, "disk")
+  type  = var.disk_type
+  size  = var.disk_size
+  image = var.disk_image
+  zone = var.region
+}
+
+resource "google_compute_address" "static-ip-address" {
+  name = format("%s-%s", google_compute_instance.instance.name, "static-ip")
+}
